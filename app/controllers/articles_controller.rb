85a4683bb80909524c9b1b747b5fd1d4d0e72b6b
Anna Louis <annalouis2@gmail.com>
2016-11-15 16:49:24 +0100

    articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,27 @@
+class ArticlesController < ApplicationController
+
+def index
+  @articles = Article.all
+end
+
+
+def new
+end
+
+
+def edit
+end
+
+
+def show
+end
+end
+
+def update
+end
+
+
+def destroy
+end
+
+
